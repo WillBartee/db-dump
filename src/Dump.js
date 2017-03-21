c7c7e1b2c753fb7582e352fe56f5bed3a6c7c6d8
Michael van der Weg <michael@joinbox.com>
2017-03-21 01:42:03 +0100

    several bugfixes

--- src/Dump.js
+++ src/Dump.js
@@ -86,6 +86,32 @@
                 }));
             }).then(() => {
                 if (!this.silent) log.success(`Loaded ${configurations.size} configurations ...`);
+                if (!this.silent) log.info(`Starting extension dumps ...`);
+
+                // set the indey
+                Array.from(configurations.values()).forEach((configuration, index) => {
+                    configuration.index = (index+10)*100;
+                });
+
+
+                // dump extensions
+                return Promise.sequence(Array.from(configurations.values()).filter(c => c.extensions && c.extensions.length).map((configuration) => {
+                    return () => {
+                        log.debug(`Dumping extensions for ${configuration.schema} ...`);
+
+                        
+                        const dataFile = path.join(this.dataDir, `${configuration.index}-${configuration.schema}.extensions.sql`);
+                        if (!this.silent) log.debug(`Extension dump for ${this.config.database}.${configuration.schema} succeeded ...`);
+
+                        fs.writeFileSync(dataFile, configuration.extensions.map(e => `create extension ${e};`).join('\n'));
+                        if (!this.silent) log.info(`Extension dump for ${this.config.database}.${configuration.schema} stored in file ${dataFile}`);
+                        
+
+                        return Promise.resolve();
+                    };            
+                }));
+            }).then(() => {
+                if (!this.silent) log.success(`Extensions for ${configurations.size} schemas dumped ...`);
                 if (!this.silent) log.info(`Starting schema dumps ...`);
 
                 // dump the structure
@@ -93,16 +119,18 @@
                     return () => {
                         log.debug(`Dumping schema of ${configuration.schema} ...`);
 
-                        configuration.index = (index+10)*100;
 
                         return new Promise((resolve, reject) => {
-                            cp.exec(`PGPASSWORD="${this.config.pass}" pg_dump --schema=${configuration.schema} -s -x --clean -U ${this.config.user} -h ${this.config.host} -p ${this.config.port} --create ${this.config.database}`, {maxBuffer: 1024*1024*20}, (err, stdOut, stdErr) => {
+                            cp.exec(`PGPASSWORD="${this.config.pass}" pg_dump -Fc --schema=${configuration.schema} -s -x --clean -U ${this.config.user} -h ${this.config.host} -p ${this.config.port} --create ${this.config.database}`, {
+                                  maxBuffer: 1024*1024*20
+                                , encoding: 'buffer'
+                            }, (err, stdOut, stdErr) => {
                                 if (err) reject(err);
                                 else {
-                                    const dataFile = path.join(this.dataDir, `${configuration.index}-${configuration.schema}.sql`);
+                                    const dataFile = path.join(this.dataDir, `${configuration.index+10}-${configuration.schema}.dump`);
                                     if (!this.silent) log.debug(`Dump for ${this.config.database}.${configuration.schema} succeeded ...`);
 
-                                    fs.writeFileSync(dataFile, stdOut.toString());
+                                    fs.writeFileSync(dataFile, stdOut);
                                     if (!this.silent) log.info(`Dump for ${this.config.database}.${configuration.schema} stored in file ${dataFile}`);
                                     resolve();
                                 }
@@ -121,13 +149,16 @@
                         log.debug(`Dumping data from ${configuration.schema} ...`);
 
                         return new Promise((resolve, reject) => {
-                            cp.exec(`PGPASSWORD="${this.config.pass}" pg_dump --schema=${configuration.schema} -a -t '"${configuration.schema}"."${configuration.dataFrom.join(`"' -t '"${configuration.schema}"."`)}"' -x -U ${this.config.user} -h ${this.config.host} -p ${this.config.port} --create ${this.config.database}`, {maxBuffer: 1024*1024*20}, (err, stdOut, stdErr) => {
+                            cp.exec(`PGPASSWORD="${this.config.pass}" pg_dump -Fc --schema=${configuration.schema} -a -t '"${configuration.schema}"."${configuration.dataFrom.join(`"' -t '"${configuration.schema}"."`)}"' -x -U ${this.config.user} -h ${this.config.host} -p ${this.config.port} --create ${this.config.database}`, {
+                                  maxBuffer: 1024*1024*20
+                                , encoding: 'buffer'
+                            }, (err, stdOut, stdErr) => {
                                 if (err) reject(err);
                                 else {
-                                    const dataFile = path.join(this.dataDir, `${configuration.index+1}-${configuration.schema}.data.sql`);
+                                    const dataFile = path.join(this.dataDir, `${configuration.index+20}-${configuration.schema}.data.dump`);
                                     if (!this.silent) log.debug(`Data dump for ${this.config.database}.${configuration.schema} succeeded ...`);
 
-                                    fs.writeFileSync(dataFile, stdOut.toString());
+                                    fs.writeFileSync(dataFile, stdOut);
                                     if (!this.silent) log.info(`Data dump for ${this.config.database}.${configuration.schema} stored in file ${dataFile}`);
                                     resolve();
                                 }
